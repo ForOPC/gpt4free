86333347b21b42bf71c2796d7a0e717e175b9295
Viacheslav <vipmodaby2@gmail.com>
2023-09-02 05:49:57 +0300

    first commit

---
+++ gui_app.py
@@ -0,0 +1,42 @@
+import logging
+from flask import Flask, render_template, request
+
+import g4f
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+# Initialize conversation
+conversation = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        conversation.append({"role": "user", "content": user_input})
+
+        request_payload = {
+            "model": "gpt-4",  # gpt-3.5-turbo
+            "provider": g4f.Provider.DeepAi,
+            "messages": conversation
+        }
+
+        response = g4f.ChatCompletion.create(**request_payload, stream=False)
+        assistant_response = response
+
+        conversation.append(
+            {"role": "assistant", "content": assistant_response})
+        logging.debug(conversation)
+
+    return render_template('gui.html', conversation=conversation)
+
+
+if __name__ == '__main__':
+    config = {
+        'host': '0.0.0.0',
+        'port': 3336,
+        'debug': True
+    }
+
+    app.run(**config)
---
+++ readme.md
@@ -0,0 +1,3 @@
+#gpt-4 DeepAi
+
+[![Gitpod](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/ForOPC/gpt4free) 
